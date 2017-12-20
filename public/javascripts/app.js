732704b365748c5eb5142a8abf2b912023e71ebb
DK <danielkim@soulkast.com>
2017-12-20 01:38:10 -0500

    added save collision prevention

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,28 +1,42 @@
-const START_MSG = 'start typing here ...';
-var app = angular.module('NoteApp', []);
-app.controller('NoteCtrl', function($scope) {
-  var jMain = $('#main');
-  var text = localStorage.getItem("text");
-  if(text === null){
-    text = START_MSG;
-  }
-  jMain.text(text);
+(function() {
+  var main = document.getElementById('main'),
+    container = main.parentNode,
+    medium = new Medium({
+      element: main,
+      mode: Medium.richMode,
+      placeholder: 'Start typing here ...',
+      attributes: null,
+      tags: null
+    });
 
-  jMain.click(function(){
-    if(text === START_MSG){
-      jMain.text('');
+  var app = angular.module('NoteApp', []);
+
+  app.controller('NoteCtrl', function($scope) {
+
+    var text = localStorage.getItem("note.soulkast.com:text");
+    if(text !== null){
+      medium.value(text);
     }
+
+    window.onbeforeunload = function() {
+      $scope.save();
+      return undefined;
+    };
+
+    var lastEdited = (new Date).getTime();
+    $('#main').keypress(function(){
+      lastEdited = (new Date).getTime();
+    });
+
+    $scope.save = function(){
+      var text = medium.value();
+      var lastSaved = parseInt(localStorage.getItem("note.soulkast.com:lastSaved"));
+      if(isNaN(lastSaved)|| lastEdited > lastSaved){
+        console.info('i am saving');
+        localStorage.setItem('note.soulkast.com:text', text);
+        localStorage.setItem("note.soulkast.com:lastSaved",lastEdited);
+      }
+    };
   });
 
-  window.onbeforeunload = function() {
-    $scope.save();
-    return undefined;
-  };
-
-  $scope.save = function(){
-    var text = jMain.text();
-    console.info('saving!');
-    localStorage.setItem('text', text);
-  };
-
-});
+})();
